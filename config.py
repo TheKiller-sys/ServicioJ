d536e46eeaed5f4adfdc2c22b82ddcfae3972c7f
Josue <61338255+TheKiller-sys@users.noreply.github.com>
2025-07-01 22:31:59 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,12 @@
-    import os
-    from dotenv import load_dotenv
+import os
+from dotenv import load_dotenv
 
-    load_dotenv()
+load_dotenv()
 
-    TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
-    ADMIN_PASSWORD = os.getenv("ADMIN_PASSWORD")
-    DATABASE_NAME = os.getenv("DATABASE_NAME", "ventas.db")
-    GROUP_ID = os.getenv("GROUP_ID")
-    UPTIMEROBOT_API_KEY = os.getenv("UPTIMEROBOT_API_KEY")
-    UPTIMEROBOT_MONITOR_ID = os.getenv("UPTIMEROBOT_MONITOR_ID")
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+ADMIN_PASSWORD = os.getenv("ADMIN_PASSWORD")
+DATABASE_NAME = os.getenv("DATABASE_NAME", "ventas.db")
+GROUP_ID = os.getenv("GROUP_ID")
+UPTIMEROBOT_API_KEY = os.getenv("UPTIMEROBOT_API_KEY")
+UPTIMEROBOT_MONITOR_ID = os.getenv("UPTIMEROBOT_MONITOR_ID")
+LOG_GROUP_ID = os.getenv("LOG_GROUP_ID")
